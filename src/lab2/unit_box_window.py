f3e73589b67c1f75329a68505a12c6adffa64d8e
KsalimK <salim@Salim.local>
2021-10-17 00:45:26 +0200

    unit_box vectorized and tested

---
+++ src/lab2/unit_box_window.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from lab2.box_window import BoxWindow
+
+
+class UnitBoxWindow(BoxWindow):
+    def __init__(self, center):
+        """a subclass of BoxWindow,represents the notion of "unit square box"
+
+        Args:
+            dimension (int): dimension of the Unit Box
+            center (array, optional): center of the Box.
+        """
+        self.bounds = np.column_stack((center - 0.5, center + 0.5))
+
+        super().__init__(self.bounds)
